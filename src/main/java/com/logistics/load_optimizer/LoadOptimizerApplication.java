4b705e1f646804140cd827574e1541f4dad73aac
Pratik <pkt9888@gmail.com>
2026-04-29 13:42:47 +0530

    Initialise Spring-Boot application

---
+++ src/main/java/com/logistics/load_optimizer/LoadOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.load_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadOptimizerApplication.class, args);
+	}
+
+}
